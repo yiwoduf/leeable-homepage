65fd25481f35865331d61496845fc8a41301da04
leeable <yiwoduf@gmail.com>
2024-02-17 23:07:47 -0600

    &apos; Fix

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 export default function PageNotFound() {
   return (
     <>
-      <h1>Can't Find The Page You Requested</h1>
+      <h1>Can&apos;t Find The Page You Requested</h1>
     </>
   );
 }
